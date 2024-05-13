cf093fd7ba69aacc83a6d9fd1b4301db78c8a570
Doze <79890783+DozeKR@users.noreply.github.com>
2024-05-13 21:02:10 +0900

    플레이어 이동구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,37 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody2D rigid;
+    [SerializeField] private float maxSpeed;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if(Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+    }
+
     void FixedUpdate()
     {
+        // Move By Key Control
         float h = Input.GetAxisRaw("Horizontal");
-
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        // 우측 최대 조건 
+        if( rigid.velocity.x > maxSpeed)
+        {
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        }
+        // 좌측 최대 조건
+        else if(rigid.velocity.x < -maxSpeed)
+        {
+            rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+        }
     }
 }
  
