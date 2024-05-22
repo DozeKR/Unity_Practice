94d69fba8ccde93c3983e0fb798c0fc0b1a63960
Doze <79890783+DozeKR@users.noreply.github.com>
2024-05-22 23:55:03 +0900

    코드 정리

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -17,7 +15,5 @@ public class GameManager : MonoBehaviour
     public void HealthDown(int damage)
     {
         playerHealth -= damage;
-
-        Debug.Log(playerHealth);
     }
 }
